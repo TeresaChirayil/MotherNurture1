a9582e2af43c00c09763cd1424c3819a74a86cdd
Teresa Chirayil <tchir3@uic.edu>
2025-11-04 09:10:12 -0600

    Initial Commit

---
+++ MotherNurture1/MotherNurture1App.swift
@@ -0,0 +1,17 @@
+//
+//  MotherNurture1App.swift
+//  MotherNurture1
+//
+//  Created by 40 GO Participant on 11/4/25.
+//
+
+import SwiftUI
+
+@main
+struct MotherNurture1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MotherNurture1Tests/MotherNurture1Tests.swift
@@ -0,0 +1,16 @@
+//
+//  MotherNurture1Tests.swift
+//  MotherNurture1Tests
+//
+//  Created by 40 GO Participant on 11/4/25.
+//
+
+import Testing
+
+struct MotherNurture1Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
