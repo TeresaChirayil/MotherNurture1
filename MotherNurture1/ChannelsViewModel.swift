b6cf2d8decd98661cc0318b710b9fb7a9d6a7efc
snowpantheraa <asiabutt05@gmail.com>
2025-12-16 18:33:35 -0600

    Matchmaking + Channels

---
+++ MotherNurture1/ChannelsViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  ChannelsViewModel.swift
+//  MotherNurture1
+//
+//  Created by 50 GO Participant on 12/16/25.
+//
+
+import SwiftUI
+
+struct ChannelsViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ChannelsViewModel()
+}
